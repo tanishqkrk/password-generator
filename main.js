f72c15ceb6e406959e3563d9451239921b4d5e26
tanishqkrk <tanishqkrk@gmail.com>
2022-09-07 18:22:07 +0530

    generator v1

--- main.js
+++ main.js
@@ -1,27 +1,30 @@
-import './style.css'
-// Selectors
-const password = document.querySelector('.app__password--container');
-const copy = document.querySelector('.app__password i');
-const range = document.querySelector('#range');
-const rangeNumber = document.querySelector('.app__main--length-value label');
-const uppercase = document.querySelector('#uppercase');
-const lowercase = document.querySelector('#lowercase');
-const numbers = document.querySelector('#numbers');
-const symbols = document.querySelector('#symbols');
-const easy = document.querySelector('#easy');
-const medium = document.querySelector('#medium');
-const hard = document.querySelector('#hard');
-const bar = document.querySelectorAll('.app__main--strength-meter-bars-bar');
-const button = document.querySelector('.app__main--button');
-const passwordArray = [];
-
+// live bar values 
+rangeBar.addEventListener('input', e => {
+  rangeNumber.innerHTML = rangeBar.value;
+  // return range;/
+})
 
 const generate = e => {
-  for (let i = 0; i < range; i++) {
-    passwordArray.push(Math.random * 9 + 0)
-  }
-  password.innerHTML = passwordArray
-}
+  const passwordArray = [];
+  const uppercaseLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+  const lowercaseLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+  const symbolLetters = ['!', '@', '#', '$', '%', '^', '&', '*'];
 
+  // For every item from 0 to the set range, the random function will create a character
+
+  for (let i = -1; i < rangeNumber.innerHTML; i++) {
+    let randomUppercase = uppercaseLetters[Math.floor(Math.random() * uppercaseLetters.length)]
+    let randomLowercase = lowercaseLetters[Math.floor(Math.random() * lowercaseLetters.length)]
+    let randomNumber = Math.floor(Math.random() * 9 + 0);
+    let randomSymbol = symbolLetters[Math.floor(Math.random() * symbolLetters.length)]
+    password.innerHTML = passwordArray.join('');
+    let masterRandom = [randomUppercase, randomLowercase, randomSymbol, randomNumber]
+
+    // Checkbox conditions
+    if (!uppercase.checked && !lowercase.checked && !numbers.checked && !symbols.checked) {
+      passwordArray.push(masterRandom[Math.floor(Math.random() * masterRandom.length)]);
+    }
+  }
+}
 button.addEventListener('click', generate);
 button.click()
